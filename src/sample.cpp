21f854ca274a9e96814c8182984f3c21ae0349ea
woods <woods@47e89930-31bd-de11-b494-001ec944baf4>
2009-10-20 04:53:56 +0000

    set empty for newer version.
    git-svn-id: file:///var/svn/bkhdrs/bkshhdr/trunk@7 47e89930-31bd-de11-b494-001ec944baf4

--- src/sample.cpp
+++
@@ -1,4 +0,0 @@
-// ウィンドウの作成(ダミーのウィンドウ)
-HWND hHiddenParentWnd = CreateWindowEx(WS_EX_TOOLWINDOW,
-            lpszMyClassName, NULL, 0, CW_USEDEFAULT, 0,
-            CW_USEDEFAULT, 0, NULL, NULL, hMyInstance, NULL);
